88824a129e655e027ee83514874485bcfd57e74f
theZMI <zmigpost@gmail.com>
2024-10-16 19:31:44 +0200

    Обновлены контакты по движку

--- tpl/contacts.php
+++ tpl/contacts.php
@@ -4,6 +4,7 @@
             <h1 class="mt-4 mb-3">Контакты</h1>
             <p>Почта: <a href="mailto:<?= SUPPORT_EMAIL ?>"><?= SUPPORT_EMAIL ?></a></p>
             <p>Телефон: <a href="tel:<?= PhoneFilter(SUPPORT_PHONE) ?>"><?= SUPPORT_PHONE ?></a></p>
+            <p>GitHub: <a href="https://github.com/theZMI/micron2">Micron2</a></p>
         </div>
     </div>
 </div>
