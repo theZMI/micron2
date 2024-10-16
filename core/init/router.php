4456308dee6ba1866c199d38b67e1103a0b6c773
theZMI <zmigpost@gmail.com>
2024-10-16 19:30:47 +0200

    Упрощён роутинг

--- core/init/router.php
+++ core/init/router.php
@@ -12,6 +12,7 @@ $getLangs = function () {
     $langs[DEF_LANG] = '';
     return $langs;
 };
+$apiUri   = 'api/v1.0/';
 
 // Админка
 SimpleRouter::group(['middleware' => \Middlewares\AdminAuthMiddleware::class], function () {
@@ -22,21 +23,44 @@ SimpleRouter::group(['middleware' => \Middlewares\AdminAuthMiddleware::class], f
 });
 
 foreach ($getLangs() as $lang => $langUri) {
+    // Вызовы API с авторизацией
+    SimpleRouter::group(['middleware' => [\Middlewares\PrepareApiMiddleware::class, \Middlewares\CommonMiddleware::class, \Middlewares\CheckApiUserAuthMiddleware::class]], function () use ($langUri, $apiUri) {
+        // users
+        SimpleRouter::all("/{$langUri}{$apiUri}users", function () use ($langUri, $apiUri) {
+            TryIncludeCom("{$langUri}{$apiUri}users", [], "{$apiUri}404");
+        })->where(['end_uri' => '.*?']);
+
+        // users/{id}
+        SimpleRouter::all("/{$langUri}{$apiUri}users/{id}", function ($id) use ($langUri, $apiUri) {
+            TryIncludeCom("{$langUri}{$apiUri}users/user", ['id' => $id], "{$apiUri}404");
+        })->where(['id' => '[0-9]+']);
+
+        // users/*
+        SimpleRouter::all("/{$langUri}{$apiUri}users{end_uri}", function ($end_uri) use ($langUri, $apiUri) {
+            $end_uri = FileSys::filenameSecurity($end_uri);
+            $end_uri = empty($end_uri) ? '' : "/{$end_uri}";
+            TryIncludeCom("{$langUri}{$apiUri}users{$end_uri}", [], "{$apiUri}404");
+        })->where(['end_uri' => '.*?']);
+    });
+
+    // Вызовы API без авторизации
+    SimpleRouter::group(['middleware' => [\Middlewares\PrepareApiMiddleware::class, \Middlewares\CommonMiddleware::class]], function () use ($langUri, $apiUri) {
+        SimpleRouter::all("/{$langUri}{$apiUri}{end_uri}", function ($end_uri) use ($langUri, $apiUri) {
+            $end_uri = FileSys::filenameSecurity($end_uri);
+            TryIncludeCom("{$langUri}{$apiUri}{$end_uri}", [], "{$apiUri}404");
+        })->where(['end_uri' => '.*?']);
+    });
+
     // Кабинет пользователя
-    SimpleRouter::group(['middleware' => \Middlewares\UserAuthMiddleware::class], function () use ($langUri) {
+    SimpleRouter::group(['middleware' => [\Middlewares\PrepareUserMiddleware::class, \Middlewares\CommonMiddleware::class, \Middlewares\CheckUserAuthMiddleware::class]], function () use ($langUri) {
         SimpleRouter::all("/{$langUri}user/{end_uri}", function ($end_uri) use ($langUri) {
             $end_uri = FileSys::filenameSecurity($end_uri);
             TryIncludeCom("/{$langUri}user/{$end_uri}");
         })->where(['end_uri' => '.*?']);
     });
 
-    // Все запросы сайта
-    SimpleRouter::group(['middleware' => \Middlewares\CommonMiddleware::class], function () use ($langUri) {
-        // *-*-* Уникальные запросы сайта необходимо вставлять вот сюда: *-*-*
-        // ... (такие запросы которые не подходят под стандартные правила микрона когда uri == подключаемым компонентам (src+tpl)
-        // *-*-* Конец уникальных запросов сайта *-*-*
-
-        // Стандартный тип подключения микрона (т.е. contacts/about_us -> IncludeCom('contacts/about_us'))
+    // Стандартный тип подключения микрона (т.е. contacts/about_us -> IncludeCom('contacts/about_us'))
+    SimpleRouter::group(['middleware' => \Middlewares\CommonMiddleware::class], function () use ($langUri, $apiUri) {
         SimpleRouter::all("/{$langUri}{end_uri}", function ($end_uri) use ($langUri) {
             $end_uri = FileSys::filenameSecurity($end_uri);
             if (in_array($end_uri, ['index.php', 'index.html', 'index.htm'])) {
@@ -45,8 +69,8 @@ foreach ($getLangs() as $lang => $langUri) {
             }
             TryIncludeCom("{$langUri}{$end_uri}");
         })->where(['end_uri' => '.*?']);
-        SimpleRouter::all("/{$langUri}", function () use ($langUri) {
-            TryIncludeCom($langUri . Config('defaultComponent'));
-        });
+
+        // Запросы вида /ru, /en... должны вызывать главную
+        SimpleRouter::all("/{$langUri}", fn() => TryIncludeCom($langUri . Config('defaultComponent')));
     });
 }
