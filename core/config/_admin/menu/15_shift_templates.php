b40f77259d5a1e3d65323e72f508cffa1c19480f
theZMI <zmigpost@gmail.com>
2024-12-18 16:04:36 +0200

    Добавляем раздел шаблонов смен

---
+++ core/config/_admin/menu/15_shift_templates.php
@@ -0,0 +1,9 @@
+<?php
+
+Config(['admin_menu', 'PUSH'], [
+    'link'  => SiteRoot('_admin/shift_templates'),
+    'name'  => 'Шаблоны смен',
+    'label' => '',
+    'css'   => '',
+    'list'  => []
+]);
