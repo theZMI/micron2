856852554f2020e233b2f92d8033e3811be6a109
theZMI <zmigpost@gmail.com>
2025-01-13 17:12:06 +0200

    Тестовый скрипт для проверки схем в моделях

--- src/_tests/model_with_scheme.php
+++ src/_tests/model_with_scheme.php
@@ -1,11 +1,10 @@
 <?php
 
-$model = new TestSchemeModel();
-$model->int_data_1 = mt_rand(1, 1000);
-$model->string_data_1 = md5( mt_rand(1, 1000) );
-$model->string_data_2 = "String value = " . md5( mt_rand(1, 1000) );
-$model->bool_data_1 = mt_rand(0, 1);
-$model->float_data_1 = 1/mt_rand(1, 10);
+$model->int_data_1    = mt_rand(1, 1000);
+$model->string_data_1 = md5(mt_rand(1, 1000));
+$model->string_data_2 = "String value = " . md5(mt_rand(1, 1000));
+$model->bool_data_1   = mt_rand(0, 1);
+$model->float_data_1  = 1 / mt_rand(1, 10);
 unset($model);
 
 $toJson = [];
