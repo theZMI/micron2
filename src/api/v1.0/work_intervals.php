ec70b346e5fa6fe4b99db79c2da73f2f8ac2ec3c
theZMI <zmigpost@gmail.com>
2025-01-17 19:40:00 +0200

    Теперь периоды возвращаются только от 0:00 текущего дня

--- src/api/v1.0/work_intervals.php
+++ src/api/v1.0/work_intervals.php
@@ -1,3 +1,10 @@
 <?php
 
-(new ApiResponse())->normal([]);
+$model = new WorkIntervalModel();
+$list  = $model->find([
+    'user_id' => $g_user->id,
+    'from'    => strtotime( date('d-m-Y 00:00:00') )
+]);
+(new ApiResponse())->normal(
+    array_map(fn($v) => $v->getData(), $list)
+);
