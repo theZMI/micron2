14167dd41199f2b15376cdc6d18051fadf7ae91b
theZMI <zmigpost@gmail.com>
2025-01-24 16:47:13 +0200

    Поиск интервала по временную id

--- src/api/v1.0/work_intervals/interval.php
+++ src/api/v1.0/work_intervals/interval.php
@@ -1,11 +1,17 @@
 <?php
 
 if (Post('is_set')) {
-    $id             = +$id > 0 ? $id : null;
+    $user_id          = +$g_user->id;
+    $start            = intval(Post('start'));
+    $stop             = intval(Post('stop'));
+    $id               = +$id > 0
+        ? $id
+        : (new WorkIntervalModel())->findOne(['user_id' => $user_id, 'temp_id' => +$id]);
+
     $model          = new WorkIntervalModel($id);
-    $model->user_id = $g_user->id;
-    $model->start   = Post('start');
-    $model->stop    = Post('stop');
+    $model->user_id = $user_id;
+    $model->start   = $start ?: $model->start;
+    $model->stop    = $stop ?: $model->stop;
     $model->flush();
 
     (new ApiResponse())->normal(
