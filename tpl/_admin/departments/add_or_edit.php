bb674f74dc1da7a69c6f3f27ec71e1e5eec3ac66
theZMI <zmigpost@gmail.com>
2025-02-17 19:08:53 +0200

    Установка таймера для отдела

--- tpl/_admin/departments/add_or_edit.php
+++ tpl/_admin/departments/add_or_edit.php
@@ -11,6 +11,12 @@
                     <label class="form-label">Название:</label>
                     <input type="text" name="department" class="form-control" value="<?= $modelParam('department') ?>" required>
                 </div>
+                <div class="form-check form-switch form-switch-lg mb-5">
+                    <input class="form-check-input" type="checkbox" name="use_timer" id="flexCheckUseTimer" role="switch">
+                    <label class="form-check-label ms-3 mt-2" for="flexCheckUseTimer">
+                        Отдел использует таймер
+                    </label>
+                </div>
                 <div class="text-center">
                     <a href="<?= SiteRoot('_admin/departments') ?>" class="btn btn-link btn-lg"><i class="bi bi-arrow-left pe-2"></i>К списку</a>
                     <button type="submit" class="btn btn-primary btn-lg rounded-pill"><i class="bi bi-check-lg pe-2"></i>Сохранить</button>
