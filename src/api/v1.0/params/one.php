cf225352452e38fbe76b050cf3b07f5cb6cfe72f
theZMI <zmigpost@gmail.com>
2025-02-18 15:20:00 +0200

    Обработка загрузки картинок

--- src/api/v1.0/params/one.php
+++ src/api/v1.0/params/one.php
@@ -11,6 +11,15 @@ if (Post('is_set')) {
         if (!in_array($k, $canEditFields)) {
             continue;
         }
+        // Если в value пришла base64 картинка, то пытаемся её сохранить
+        if ($k === 'value' && preg_match('/^data:image\/(\w+);base64,/', $v)) {
+            try {
+                $model->$k = SaveImageFromBase64(strval($v), "/upl/param_photos/" . +$id);
+            } catch (\Throwable $exception) {
+            }
+            continue;
+        }
+        // Во всех остальных случаях просто сохраняем поле которое есть
         $model->$k = $v;
     }
 }
