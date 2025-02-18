a53bde88d14653b1d6a76a6262e519f998db0b88
theZMI <zmigpost@gmail.com>
2025-02-18 20:18:37 +0200

    Мульти прогресс-бары (сколько выполнено и провалено)

--- tpl/_admin/reports.php
+++ tpl/_admin/reports.php
@@ -10,6 +10,7 @@
                         <tr>
                             <th>ID</th>
                             <th>Название</th>
+                            <th>Статус</th>
                             <th width="1%">Прогресс по задачам</th>
                             <th colspan="2" style="display: none;">Действия</th>
                         </tr>
@@ -17,11 +18,19 @@
                             <tr>
                                 <td onclick="trClick(this)"><?= $v->id ?></td>
                                 <td onclick="trClick(this)"><?= $v->name ?></td>
+                                <td onclick="trClick(this)"><?= $v->status_name !== (new ShiftModel())->statuses(ShiftModel::STATUS_CREATED)['name'] ? $v->status_name : '' ?></td>
                                 <td>
                                     <div class="d-flex justify-content-center align-items-center">
-                                        <div class="progress" style="width: 300px;">
-                                            <div class="progress-bar progress-bar-striped" role="progressbar" style="width: <?= intval($v->progress['percent']) ?>%" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100">
-                                                <?= intval($v->progress['done']) ?> / <?= intval($v->progress['total']) ?>
+                                        <div class="progress-stacked" style="width: 300px;">
+                                            <div class="progress" role="progressbar" style="width: <?= intval($v->progress['percent_done']) ?>%" aria-valuenow="<?= intval($v->progress['percent_done']) ?>" aria-valuemin="0" aria-valuemax="100">
+                                                <div class="progress-bar progress-bar-striped">
+                                                    <?= intval($v->progress['done']) ?> / <?= intval($v->progress['total']) ?>
+                                                </div>
+                                            </div>
+                                            <div class="progress" role="progressbar" style="width: <?= intval($v->progress['percent_failed']) ?>%" aria-valuenow="<?= intval($v->progress['percent_failed']) ?>" aria-valuemin="0" aria-valuemax="100">
+                                                <div class="progress-bar progress-bar-striped bg-danger">
+                                                    <?= intval($v->progress['failed']) ?>
+                                                </div>
                                             </div>
                                         </div>
                                         <button type="button" class="btn btn-sm btn-link float-end more-info" title="Подробнее...">
@@ -34,9 +43,16 @@
                                             <tr>
                                                 <td class="border border-0 text-nowrap text-end pe-3"><?= $shift->user->full_name ?></td>
                                                 <td class="border border-0">
-                                                    <div class="progress" style="width: 125px;">
-                                                        <div class="progress-bar progress-bar-striped bg-secondary" role="progressbar" style="width: <?= intval($shift->progress['percent']) ?>%" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100">
-                                                            <?= intval($shift->progress['done']) ?> / <?= intval($shift->progress['total']) ?>
+                                                    <div class="progress-stacked" style="width: 125px;">
+                                                        <div class="progress" role="progressbar" style="width: <?= intval($shift->progress['percent_done']) ?>%" aria-valuenow="<?= intval($shift->progress['percent_done']) ?>" aria-valuemin="0" aria-valuemax="100">
+                                                            <div class="progress-bar progress-bar-striped">
+                                                                <?= intval($shift->progress['done']) ?> / <?= intval($v->progress['total']) ?>
+                                                            </div>
+                                                        </div>
+                                                        <div class="progress" role="progressbar" style="width: <?= intval($shift->progress['percent_failed']) ?>%" aria-valuenow="<?= intval($shift->progress['percent_failed']) ?>" aria-valuemin="0" aria-valuemax="100">
+                                                            <div class="progress-bar progress-bar-striped bg-danger">
+                                                                <?= intval($shift->progress['failed']) ?>
+                                                            </div>
                                                         </div>
                                                     </div>
                                                 </td>
@@ -49,7 +65,7 @@
                                     <a href="<?= SiteRoot("_admin/dir_shifts/report&id={$id}") ?>" class="btn btn-sm btn-secondary rounded-pill" title="Отчёт"><i class="bi bi-ui-checks"></i></a>
                                 </td>
                                 <td width="1%" class="text-center">
-                                    <a href="<?= SiteRoot("_admin/dir_shifts/close&id={$id}") ?>" class="btn btn-sm btn-primary rounded-pill text-nowrap" title="Закрыть смену" onclick="return confirm('Вы уверены что хотите закрыть смену?')">Закрыть смену</a>
+                                    <a href="<?= SiteRoot("_admin/dir_shifts/close&id={$id}") ?>" class="btn btn-sm btn-danger rounded-pill text-nowrap" title="Закрыть смену" onclick="return confirm('Вы уверены что хотите закрыть смену?')">Закрыть смену</a>
                                 </td>
                             </tr>
                         <?php endforeach; ?>
