234f70b974ea4dd5a9471ce38c0224e196d7cdbe
theZMI <zmigpost@gmail.com>
2025-02-18 13:23:56 +0200

    Обновляем формат таймера, теперь HH:MM везде на сайте и в приложении

--- i/js/_dev/format_date.js
+++ i/js/_dev/format_date.js
@@ -8,12 +8,12 @@ export function formatDateTime(timestamp) {
     return `${dt.getDate().toString().padStart(2, '0')}-${(dt.getMonth() + 1).toString().padStart(2, '0')}-${dt.getFullYear().toString().padStart(4, '0')} ${dt.getHours().toString().padStart(2, '0')}:${dt.getMinutes().toString().padStart(2, '0')}:${dt.getSeconds().toString().padStart(2, '0')}`;
 }
 
-export function formatTimer(timeInSeconds) {
+export function formatTimer(timeInSeconds, withSeconds = true) {
     const dt = new Date(timeInSeconds * 1000);
-    const h = dt.getUTCHours().toString().padStart(2, '');
+    const h = dt.getUTCHours().toString().padStart(2, '0');
     const m = dt.getUTCMinutes().toString().padStart(2, '0');
-    const s = dt.getUTCSeconds().toString().padStart(2, '0');
-    return (+h > 0 ? `${h}:` : '') + [m, s].join(':');
+    const s = withSeconds ? dt.getUTCSeconds().toString().padStart(2, '0') : '';
+    return (+h > 0 ? `${h}:` : '') + [m, s].filter(v => !!v).join(':');
 }
 
 export function getNow() {
