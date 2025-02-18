1d2c40e8a0e36d55d37195ee327a55a832728521
theZMI <zmigpost@gmail.com>
2025-02-18 19:03:34 +0200

    Возможность кнопки "подробнее"

--- i/js/_admin/main.js
+++ i/js/_admin/main.js
@@ -9,3 +9,14 @@ function trClick(td) {
 
     location.href = gotoLink;
 }
+
+$(() => {
+    $('.more-info').on('click', function() {
+        const btn = this;
+        $(btn).css(
+            'transform',
+            $(btn).css('transform') === 'none' ? 'rotate(180deg)' : ''
+        );
+        $(btn).parent().next('.more-info').toggle();
+    })
+});
--- tpl/_admin/reports.php
+++ tpl/_admin/reports.php
@@ -5,12 +5,12 @@
                 <span class="pull-start me-3">Отчёты</span>
             </h1>
             <div class="table-responsive mb-4 table-extra-condensed-wrapper">
-                <table class="table table-condensed table-hover table-extra-condensed">
+                <table class="table table-condensed table-extra-condensed">
                     <?php if (count($list)): ?>
                         <tr>
                             <th>ID</th>
                             <th>Название</th>
-                            <th width="220">Прогресс по задачам</th>
+                            <th width="1%">Прогресс по задачам</th>
                             <th colspan="2" style="display: none;">Действия</th>
                         </tr>
                         <?php foreach ($list as $id => $v): ?>
@@ -18,8 +18,31 @@
                                 <td onclick="trClick(this)"><?= $v->id ?></td>
                                 <td onclick="trClick(this)"><?= $v->name ?></td>
                                 <td>
-                                    <div class="progress">
-                                        <div class="progress-bar progress-bar-striped" role="progressbar" style="width: <?= intval($v->progress) ?>%" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100"></div>
+                                    <div class="d-flex justify-content-center align-items-center">
+                                        <div class="progress" style="width: 300px;">
+                                            <div class="progress-bar progress-bar-striped" role="progressbar" style="width: <?= intval($v->progress['percent']) ?>%" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100">
+                                                <?= intval($v->progress['done']) ?> / <?= intval($v->progress['total']) ?>
+                                            </div>
+                                        </div>
+                                        <button type="button" class="btn btn-sm btn-link float-end more-info" title="Подробнее...">
+                                            <i class="bi bi-arrow-down"></i>
+                                        </button>
+                                    </div>
+                                    <div class="more-info pt-2 pb-2" style="display: none;">
+                                        <table class="table table-borderless border border-0 mb-0">
+                                            <?php foreach ($v->shifts as $shift): ?>
+                                            <tr>
+                                                <td class="border border-0 text-nowrap text-end pe-3"><?= $shift->user->full_name ?></td>
+                                                <td class="border border-0">
+                                                    <div class="progress" style="width: 125px;">
+                                                        <div class="progress-bar progress-bar-striped bg-secondary" role="progressbar" style="width: <?= intval($shift->progress['percent']) ?>%" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100">
+                                                            <?= intval($shift->progress['done']) ?> / <?= intval($shift->progress['total']) ?>
+                                                        </div>
+                                                    </div>
+                                                </td>
+                                            </tr>
+                                            <?php endforeach; ?>
+                                        </table>
                                     </div>
                                 </td>
                                 <td width="1%" class="text-center" style="display: none;">
