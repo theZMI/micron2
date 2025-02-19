b9a4a3b5a36f6d339b27a2e9e3c9400d31a6f7da
theZMI <zmigpost@gmail.com>
2025-02-19 22:25:15 +0200

    XL-таблицы для шаблонов смен

--- tpl/_admin/dir_shift_templates.php
+++ tpl/_admin/dir_shift_templates.php
@@ -7,33 +7,18 @@
                     <i class="bi bi-plus-lg me-2"></i>Добавить
                 </a>
             </h1>
-            <div class="table-responsive mb-4 table-extra-condensed-wrapper">
-                <table class="site-table">
-                    <?php if (count($list)): ?>
-                        <tr>
-                            <th>ID</th>
-                            <th>Название</th>
-                            <th colspan="2">Действия</th>
-                        </tr>
-                        <?php foreach ($list as $id => $v): ?>
-                            <tr>
-                                <td onclick="trClick(this)"><?= $v->id ?></td>
-                                <td onclick="trClick(this)"><?= $v->name ?></td>
-                                <td width="1%" class="text-center" style="display: none;">
-                                    <a href="<?= SiteRoot("_admin/dir_shift_templates/add_or_edit&id={$id}") ?>" class="btn btn-sm btn-primary rounded-pill default-click" title="Изменить данные"><i class="bi bi-pencil-fill"></i></a>
-                                </td>
-                                <td width="1%" class="text-center">
-                                    <a href="<?= GetCurUrl('a=delete&id=' . $v->id) ?>" class="btn btn-sm btn-danger rounded-pill" onclick="return confirm('Удалить?')" title="Удалить"><i class="bi bi-trash3-fill"></i></a>
-                                </td>
-                            </tr>
-                        <?php endforeach; ?>
-                    <?php else: ?>
-                        <tr>
-                            <td class="text-center">Нет данных</td>
-                        </tr>
-                    <?php endif ?>
-                </table>
-            </div>
+            <?php
+            IncludeCom('_dev/xl_table', [
+                'tableHeader'           => $tableHeader,
+                'tableHeaderEnd'        => $tableHeaderEnd,
+                'tableHeaderEndFilters' => $tableHeaderEndFilters,
+                'colWidths'             => $colWidths,
+                'tableData'             => $tableData,
+                'tableDataEnd'          => $tableDataEnd,
+                'defaultTableValues'    => $defaultTableValues,
+                'tableFilters'          => $tableFilters
+            ]);
+            ?>
         </div>
     </div>
 </div>
