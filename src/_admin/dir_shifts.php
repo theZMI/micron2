77dc9cc61950927c0f8ce93fdca0285d40d2d343
theZMI <zmigpost@gmail.com>
2025-02-19 21:04:43 +0200

    Вид xl-таблиц для раздела смен теперь на основе общего компонента _dev/xl_table

--- src/_admin/dir_shifts.php
+++ src/_admin/dir_shifts.php
@@ -1,16 +1,13 @@
 <?php
 
-$page        = intval(Get('p', 1));
-$action      = Get('a');
-$list        = (new DirShiftsModel())->getList();
-$tableHeader = [
-    'ID',
-    'Название',
-    'Начать в',
-    'Завершить в',
-    'Статус',
-];
-$tableData = [];
+$action                = Get('a');
+$list                  = (new DirShiftsModel())->getList();
+$tableHeader           = ['ID', 'Название', 'Начать в', 'Завершить в', 'Статус',];
+$tableHeaderEnd        = '<th width="1%">Прогресс по задачам</th><th width="1%" colspan="2">Действия</th>';
+$tableHeaderEndFilters = '<td></td><td colspan="2"></td>';
+$colWidths             = ['ID' => '50', 'Название' => 'auto'];
+$tableData             = [];
+$tableDataEnd          = [];
 foreach ($list as $k => $v) {
     $tableData[$v->id] = [
         $v->id,
@@ -19,11 +16,24 @@ foreach ($list as $k => $v) {
         OutputFormats::dateTime($v->shifts[0]->end_time, false) . ' &nbsp; <span class="badge text-white text-bg-secondary">' . intval($v->shifts[0]->end_time - $v->shifts[0]->start_time + 1) / 86400 . ' д.</span>',
         $v->status_name,
     ];
+    ob_start();
+    ?>
+        <td class="text-nowrap ps-2 pe-2">
+            <?php IncludeCom('_admin/_dir_shift_progresses', ['model' => $v]) ?>
+        </td>
+        <td width="1%" class="text-center" style="display: none;">
+            <a href="<?= SiteRoot("_admin/dir_shifts/add_or_edit&id={$v->id}") ?>" class="btn btn-sm btn-primary rounded-pill default-click" title="Изменить данные"><i class="bi bi-pencil-fill"></i></a>
+        </td>
+        <td width="1%" class="text-center text-nowrap">
+            <a href="<?= GetCurUrl('a=delete&id=' . $v->id) ?>" class="btn btn-sm btn-danger rounded-pill" onclick="return confirm('Удалить?')" title="Удалить"><i class="bi bi-trash3-fill"></i></a>
+        </td>
+    <?php
+    $tableDataEnd[$v->id] = ob_get_clean();
 }
 $defaultTableValues = [
     'Статус' => strip_tags( (new ShiftModel())->statuses(ShiftModel::STATUS_CREATED)['name'] ),
 ];
-$tableFilters = [ // Если поле указано здесь, то будет выбран указанный тип ввода, иначе определяться будет автоматически
+$tableFilters = [
     'Начать в'    => 'input',
     'Завершить в' => 'input',
 ];
--- tpl/_admin/dir_shifts.php
+++ tpl/_admin/dir_shifts.php
@@ -1,37 +1,3 @@
-<head>
-    <script type="text/javascript">
-        let g_searchers;
-
-        $(() => {
-            <?php $countSearchers = count($tableHeader); ?>
-            <?php for ($i = 1; $i <= $countSearchers; $i++): ?>
-            $('#table-with-filters').liveFilter(
-                '.live-filter-input-<?= $i ?>',
-                '.live-filter-data-block',
-                {
-                    filterChildSelector: '.live-filter-data-block-filterinfo-<?= $i ?>',
-                    before: function (elem, contains, containsNot) {
-                        let searchers = {};
-                        let selector = '';
-                        <?php for ($j = 1; $j <= $countSearchers; $j++): ?>
-                        selector = '#table-with-filters .live-filter-input-<?= $j ?>';
-                        if ($(selector).val()) {
-                            searchers[<?= $j ?>] = $(selector).val();
-                        }
-                        <?php endfor ?>
-                        g_searchers = searchers;
-                    },
-                    after: function (elem, contains, containsNot) {
-                    }
-                }
-            );
-            <?php endfor; ?>
-
-            $('#table-with-filters').find('input:first, select:first').trigger('change');
-        });
-    </script>
-</head>
-
 <div class="container-fluid mb-4">
     <div class="row">
         <div class="col">
@@ -47,89 +13,18 @@
                     </ul>
                 </div>
             </h1>
-            <div id="table-with-filters" class="table-responsive mb-4 table-extra-condensed-wrapper">
-                <table class="site-table">
-                    <?php if (count($list)): ?>
-                    <thead>
-                        <tr>
-                            <?php $i = 1; foreach ($tableHeader as $v): ?>
-                                <th width="<?= $i === 2 ? '' : '1%'?>"><?= $v ?></th>
-                            <?php $i++; endforeach; ?>
-                            <th width="1%">Прогресс по задачам</th>
-                            <th colspan="2">Действия</th>
-                        </tr>
-                        <tr>
-                            <?php $i = 1; foreach ($tableHeader as $v): ?>
-                                <td>
-                                    <?php
-                                    // Получаем все возможные значения в этом столбце
-                                    $cells = array_map(
-                                        fn($row) => strip_tags(array_values($row)[$i-1]),
-                                        array_values($tableData)
-                                    );
-                                    $cells = array_unique($cells);
-                                    $uniq  = count($cells);
-                                    $total = count(array_column($tableData, $i-1));
-                                    natsort($cells);
-
-                                    $isEmpty = !count(array_filter($cells));
-                                    $filter  = $tableFilters[$v] ?? 'auto';
-                                    if ($filter === 'auto')
-                                    {
-                                        $isSelect = $uniq <= 10 && $uniq < $total // Показывать select если вариантов не больше 10-ти
-                                            ||
-                                            ($uniq <= 20 && $uniq <= 0.33*$total && $uniq < $total); // Если select сокращает кол-во пунктов до 1/3 от общего числа вариантов, но не больше 20
-                                    } else {
-                                        $isSelect = $filter === 'select';
-                                    }
-                                    ?>
-                                    <?php if ($isEmpty): ?>
-                                    <?php elseif ($isSelect): ?>
-                                        <select class="form-control input-for-live-search live-filter-input-<?= $i ?>">
-                                            <option value="">Неважно</option>
-                                            <?php foreach ($cells as $cell): ?>
-                                                <option value="begin_<?= $cell ?>_end"<?= isset($defaultTableValues[$v]) && (strip_tags($defaultTableValues[$v]) == $cell) ? ' selected="selected"' : '' ?>><?= $cell ?></option>
-                                            <?php endforeach; ?>
-                                        </select>
-                                    <?php else: ?>
-                                        <input type="text" class="form-control input-for-live-search live-filter-input-<?= $i ?>" placeholder="" value="<?= $defaultTableValues[$v] ?? '' ?>" />
-                                    <?php endif; ?>
-                                </td>
-                            <?php $i++; endforeach; ?>
-                            <td></td>
-                            <td colspan="2"></td>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php foreach ($tableData as $id => $row): ?>
-                            <tr class="live-filter-data-block">
-                                <?php $i = 1; foreach ($row as $cell): ?>
-                                    <td onclick="trClick(this)" class="<?= $i === 2 ? '' : 'text-nowrap' ?> ps-2 pe-2">
-                                        <?= $cell ?>
-                                        <div class="hidden d-none live-filter-data-block-filterinfo-<?= $i ?>">begin_<?= strip_tags($cell) ?>_end</div>
-                                    </td>
-                                <?php $i++; endforeach; ?>
-                                <td class="text-nowrap ps-2 pe-2">
-                                    <?php IncludeCom('_admin/_dir_shift_progresses', ['model' => $list[$id]]) ?>
-                                </td>
-                                <td width="1%" class="text-center" style="display: none;">
-                                    <a href="<?= SiteRoot("_admin/dir_shifts/add_or_edit&id={$id}") ?>" class="btn btn-sm btn-primary rounded-pill default-click" title="Изменить данные"><i class="bi bi-pencil-fill"></i></a>
-                                </td>
-                                <td width="1%" class="text-center text-nowrap">
-                                    <a href="<?= GetCurUrl('a=delete&id=' . $id) ?>" class="btn btn-sm btn-danger rounded-pill" onclick="return confirm('Удалить?')" title="Удалить"><i class="bi bi-trash3-fill"></i></a>
-                                </td>
-                            </tr>
-                        <?php endforeach; ?>
-                    </tbody>
-                    <?php else: ?>
-                    <tbody>
-                        <tr>
-                            <td class="text-center">Нет данных</td>
-                        </tr>
-                    </tbody>
-                    <?php endif ?>
-                </table>
-            </div>
+            <?php
+                IncludeCom('_dev/xl_table', [
+                    'tableHeader'           => $tableHeader,
+                    'tableHeaderEnd'        => $tableHeaderEnd,
+                    'tableHeaderEndFilters' => $tableHeaderEndFilters,
+                    'colWidths'             => $colWidths,
+                    'tableData'             => $tableData,
+                    'tableDataEnd'          => $tableDataEnd,
+                    'defaultTableValues'    => $defaultTableValues,
+                    'tableFilters'          => $tableFilters
+                ]);
+            ?>
         </div>
     </div>
 </div>
