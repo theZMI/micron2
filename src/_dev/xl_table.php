4c122bdc674a661852dd241faeb7fd12de87d642
theZMI <zmigpost@gmail.com>
2025-02-19 21:00:31 +0200

    Более удобная заготовка для XL-таблиц

---
+++ src/_dev/xl_table.php
@@ -0,0 +1,19 @@
+<?php
+
+// $tableHeader - массив заголовков таблицы
+// $tableHeaderEnd - html контент в конце headers
+// $tableHeaderEndFilters - html контент в конце фильтров у header-ов
+// $colWidths = ['ID' => '100', 'Название' => 'auto'] - определяет ширину колонок (по умолчанию каждая колонка - это 1% но без переноса)
+// $tableData - массив данных таблицы
+// $tableDataEnd - массив html контент-ов в конце таблицы (добавленные столбцы у каждой строки, формат [N-строки соответсвующий $tableData[N] => html_строка, ...]
+// $defaultTableValues - дефолтный выбор в фильтрах
+// $tableFilters - какие типы фильтров у каждого столбца (input, select, auto)
+
+if (!isset($tableHeader)) throw new RuntimeException("tableHeader must be set");
+$tableHeaderEnd        = $tableHeaderEnd ?? '';
+$tableHeaderEndFilters = $tableHeaderEndFilters ?? '';
+$colWidths             = $colWidths ?? [];
+if (!isset($tableData)) throw new RuntimeException("tableData must be set");
+$tableDataEnd          = $tableDataEnd ?? '';
+$defaultTableValues    = $defaultTableValues ?? [];
+$tableFilters          = $tableFilters ?? [];
---
+++ tpl/_dev/xl_table.php
@@ -0,0 +1,109 @@
+<head>
+    <script type="text/javascript">
+        let g_searchers;
+
+        $(() => {
+            <?php $countSearchers = count($tableHeader); ?>
+            <?php for ($i = 1; $i <= $countSearchers; $i++): ?>
+            $('#table-with-filters').liveFilter(
+                '.live-filter-input-<?= $i ?>',
+                '.live-filter-data-block',
+                {
+                    filterChildSelector: '.live-filter-data-block-filterinfo-<?= $i ?>',
+                    before: function (elem, contains, containsNot) {
+                        let searchers = {};
+                        let selector = '';
+                        <?php for ($j = 1; $j <= $countSearchers; $j++): ?>
+                        selector = '#table-with-filters .live-filter-input-<?= $j ?>';
+                        if ($(selector).val()) {
+                            searchers[<?= $j ?>] = $(selector).val();
+                        }
+                        <?php endfor ?>
+                        g_searchers = searchers;
+                    },
+                    after: function (elem, contains, containsNot) {
+                    }
+                }
+            );
+            <?php endfor; ?>
+
+            $('#table-with-filters').find('input:first, select:first').trigger('change');
+        });
+    </script>
+</head>
+
+
+<div id="table-with-filters" class="table-responsive mb-4 table-extra-condensed-wrapper">
+    <table class="site-table">
+        <?php if (count($tableData)): ?>
+            <thead>
+            <tr>
+                <?php $i = 1; foreach ($tableHeader as $v): ?>
+                    <th width="<?= $colWidths[$v] ?? '1%' ?>"><?= $v ?></th>
+                <?php $i++; endforeach; ?>
+                <?= $tableHeaderEnd ?>
+            </tr>
+            <tr>
+                <?php $i = 1; foreach ($tableHeader as $v): ?>
+                    <td>
+                        <?php
+                        // Получаем все возможные значения в этом столбце
+                        $cells = array_map(
+                            fn($row) => strip_tags(array_values($row)[$i-1]),
+                            array_values($tableData)
+                        );
+                        $cells = array_unique($cells);
+                        $uniq  = count($cells);
+                        $total = count(array_column($tableData, $i-1));
+                        natsort($cells);
+
+                        $isEmpty = !count(array_filter($cells));
+                        $filter  = $tableFilters[$v] ?? 'auto';
+                        if ($filter === 'auto')
+                        {
+                            $isSelect = $uniq <= 10 && $uniq < $total // Показывать select если вариантов не больше 10-ти
+                                        ||
+                                        ($uniq <= 20 && $uniq <= 0.33*$total && $uniq < $total); // Если select сокращает кол-во пунктов до 1/3 от общего числа вариантов, но не больше 20
+                        } else {
+                            $isSelect = $filter === 'select';
+                        }
+                        ?>
+                        <?php if ($isEmpty): ?>
+                        <?php elseif ($isSelect): ?>
+                            <select class="form-control input-for-live-search live-filter-input-<?= $i ?>">
+                                <option value="">Неважно</option>
+                                <?php foreach ($cells as $cell): ?>
+                                    <option value="begin_<?= $cell ?>_end"<?= isset($defaultTableValues[$v]) && (strip_tags($defaultTableValues[$v]) == $cell) ? ' selected="selected"' : '' ?>><?= $cell ?></option>
+                                <?php endforeach; ?>
+                            </select>
+                        <?php else: ?>
+                            <input type="text" class="form-control input-for-live-search live-filter-input-<?= $i ?>" placeholder="" value="<?= $defaultTableValues[$v] ?? '' ?>" />
+                        <?php endif; ?>
+                    </td>
+                <?php $i++; endforeach; ?>
+                <?= $tableHeaderEndFilters ?>
+            </tr>
+            </thead>
+            <tbody>
+            <?php  foreach ($tableData as $id => $row): ?>
+                <tr class="live-filter-data-block">
+                    <?php $i = 1; foreach ($row as $cell): ?>
+                        <?php $colName = $tableHeader[$i-1]; ?>
+                        <td onclick="trClick(this)" class="<?= isset($colWidths[$colName]) ? Translit("col-class-{$colName}") : 'text-nowrap' ?> ps-2 pe-2">
+                            <?= $cell ?>
+                            <div class="hidden d-none live-filter-data-block-filterinfo-<?= $i ?>">begin_<?= strip_tags($cell) ?>_end</div>
+                        </td>
+                    <?php $i++; endforeach; ?>
+                    <?= $tableDataEnd[$id] ?>
+                </tr>
+            <?php endforeach; ?>
+            </tbody>
+        <?php else: ?>
+            <tbody>
+                <tr>
+                    <td class="text-center">Нет данных</td>
+                </tr>
+            </tbody>
+        <?php endif ?>
+    </table>
+</div>
