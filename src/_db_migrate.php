972a35f08b117834388f4d6d98d7a8325fc2da71
theZMI <zmigpost@gmail.com>
2025-02-20 21:55:58 +0200

    Очищаем миграционный лист

--- src/_db_migrate.php
+++ src/_db_migrate.php
@@ -2,9 +2,7 @@
 
 $db = GetDefaultDb();
 $qs = [
-    'TABLE `shifts` DROP `_work_intervals`',
-    'ALTER TABLE `shifts` DROP `work_time`',
-    'ALTER TABLE `shift_params` ADD `status` INT NULL DEFAULT NULL AFTER `value_as_number`'
+
 ];
 array_walk($qs, fn($q) => $db->query($q));
 die("END");
