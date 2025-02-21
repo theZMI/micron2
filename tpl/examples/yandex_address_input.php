6ddbbc7eb4a353e778bc6386b6f3704899ac3646
theZMI <zmigpost@gmail.com>
2025-02-21 18:52:02 +0200

    Пример определялки местоположения от яндекс-ка (с запросом, но не на локалке)

--- tpl/examples/yandex_address_input.php
+++ tpl/examples/yandex_address_input.php
@@ -1,7 +1,7 @@
 <script src="<?= Root('i/js/_dev/yandex-address-input.js') ?>"></script>
 <script>
     $(function () {
-        initMap(ymaps, 51.507351, -0.127696);
+        initMap(ymaps);//initMap(ymaps, 51.507351, -0.127696);
     });
 </script>
 <script src="https://api-maps.yandex.ru/2.1/?apikey=215f771a-e251-4871-b409-b1dfb06ed996&suggest_apikey=45ccec0e-0618-4571-b0fa-9e5ec4a080ad&lang=ru_RU&mode=debug&load=package.full&onload=mscDistance.Ymaps.ready&ns=ymaps" type="text/javascript"></script>
