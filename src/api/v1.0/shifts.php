d7c62140fb9a36bf49885ce3cc1f7d1f9a64bfda
theZMI <zmigpost@gmail.com>
2025-02-22 04:15:19 +0200

    Учитываем status при извлечении задач/параметров по активным сменам сотрудника

--- src/api/v1.0/shifts.php
+++ src/api/v1.0/shifts.php
@@ -1,8 +1,16 @@
 <?php
 
-$dirs = (new DirShiftsModel())->find(['user_id' => +$g_user->id, 'is_template' => false]);
+$dirs = (new DirShiftsModel())->find([
+    'is_template' => false,
+    'user_id'     => +$g_user->id,
+    'status'      => ShiftModel::STATUS_CREATED,
+]);
 $all  = array_values( array_map(
-    fn($dirModel) => (new ShiftModel())->findOne(['dir_id' => $dirModel->id, 'user_id' => +$g_user->id]),
+    fn($dirModel) => (new ShiftModel())->findOne([
+        'dir_id'  => $dirModel->id,
+        'user_id' => +$g_user->id,
+        'status'  => ShiftModel::STATUS_CREATED,
+    ]),
     $dirs
 ) );
 $all  = array_map(fn($v) => $v->getDataToApi(), $all);
