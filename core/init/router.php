864c4a4cb03af3d67be3487cef46f7fe003558bb
theZMI <zmigpost@gmail.com>
2025-03-05 00:16:13 +0200

    Удаляем работу с менеджером. Теперь будут юзеры (два типа: пользователи и инженеры) и администраторы с правами доступа к разделам и группам пользователей (для работы компаний/принципиалов)

--- core/init/router.php
+++ core/init/router.php
@@ -31,15 +31,6 @@ foreach ($getLangs() as $lang => $langUri) {
         CommonMiddleware::class,
         CheckApiUserAuthMiddleware::class] // Юзер определиться по API-User-Auth-Login и будет занесён в g_user
     ], function () use ($langUri, $apiUri) {
-        // work_intervals
-        SimpleRouter::all("/{$langUri}{$apiUri}work_intervals", function () use ($langUri, $apiUri) {
-            TryIncludeCom("{$langUri}{$apiUri}work_intervals", [], "{$apiUri}404");
-        });
-        // work_intervals/{id}
-        SimpleRouter::all("/{$langUri}{$apiUri}work_intervals/{id}", function ($id) use ($langUri, $apiUri) {
-            TryIncludeCom("{$langUri}{$apiUri}work_intervals/one", ['id' => $id], "{$apiUri}404");
-        })->where(['id' => '-?[0-9]+?']);
-
         // users
         SimpleRouter::all("/{$langUri}{$apiUri}users", function () use ($langUri, $apiUri) {
             TryIncludeCom("{$langUri}{$apiUri}users", [], "{$apiUri}404");
@@ -54,26 +45,8 @@ foreach ($getLangs() as $lang => $langUri) {
             $end_uri = empty($end_uri) ? '' : "/{$end_uri}";
             TryIncludeCom("{$langUri}{$apiUri}users{$end_uri}", [], "{$apiUri}404");
         })->where(['end_uri' => '.*?']);
-
-        // shifts
-        SimpleRouter::all("/{$langUri}{$apiUri}shifts", function () use ($langUri, $apiUri) {
-            TryIncludeCom("{$langUri}{$apiUri}shifts", [], "{$apiUri}404");
-        });
-        // shifts/{id}
-        SimpleRouter::all("/{$langUri}{$apiUri}shifts/{id}", function ($id) use ($langUri, $apiUri) {
-            TryIncludeCom("{$langUri}{$apiUri}shifts/one", ['id' => $id], "{$apiUri}404");
-        })->where(['id' => '-?[0-9]+?']);
-
-        // tasks/{id}
-        SimpleRouter::all("/{$langUri}{$apiUri}tasks/{id}", function ($id) use ($langUri, $apiUri) {
-            TryIncludeCom("{$langUri}{$apiUri}tasks/one", ['id' => $id], "{$apiUri}404");
-        })->where(['id' => '-?[0-9]+?']);
-
-        // params/{id}
-        SimpleRouter::all("/{$langUri}{$apiUri}params/{id}", function ($id) use ($langUri, $apiUri) {
-            TryIncludeCom("{$langUri}{$apiUri}params/one", ['id' => $id], "{$apiUri}404");
-        })->where(['id' => '-?[0-9]+?']);
     });
+
     // Вызовы API без авторизации
     SimpleRouter::group([
         'middleware' => [
@@ -98,18 +71,6 @@ foreach ($getLangs() as $lang => $langUri) {
         })->where(['end_uri' => '.*?']);
     });
 
-    // Кабинет менеджера
-    SimpleRouter::group(['middleware' => [
-        PrepareManagerMiddleware::class,
-        CommonMiddleware::class,
-        CheckManagerAuthMiddleware::class
-    ]], function () use ($langUri) {
-        SimpleRouter::all("/{$langUri}manager/{end_uri}", function ($end_uri) use ($langUri) {
-            $end_uri = FileSys::filenameSecurity($end_uri);
-            TryIncludeCom("/{$langUri}manager/{$end_uri}");
-        })->where(['end_uri' => '.*?']);
-    });
-
     // Стандартный тип подключения микрона (т.е. contacts/about_us -> IncludeCom('contacts/about_us'))
     SimpleRouter::group(['middleware' => [
         CommonMiddleware::class
@@ -117,13 +78,13 @@ foreach ($getLangs() as $lang => $langUri) {
         SimpleRouter::all("/{$langUri}{end_uri}", function ($end_uri) use ($langUri) {
             $end_uri = FileSys::filenameSecurity($end_uri);
             if (in_array($end_uri, ['index.php', 'index.html', 'index.htm'])) {
-                IncludeCom($langUri . Config('defaultComponent'));
+                IncludeCom($langUri . Config('default_component'));
                 return;
             }
             TryIncludeCom("{$langUri}{$end_uri}");
         })->where(['end_uri' => '.*?']);
 
         // Запросы вида /ru, /en... должны вызывать главную
-        SimpleRouter::all("/{$langUri}", fn() => TryIncludeCom($langUri . Config('defaultComponent')));
+        SimpleRouter::all("/{$langUri}", fn() => TryIncludeCom($langUri . Config('default_component')));
     });
 }
