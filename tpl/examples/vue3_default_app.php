72b1968ad1e53f7ff1a6c79a232073be5ce0a949
theZMI <zmigpost@gmail.com>
2025-03-05 00:13:44 +0200

    Новые примеры работы с vue-приложением

---
+++ tpl/examples/vue3_default_app.php
@@ -0,0 +1,43 @@
+<h1 class="mt-4 mb-4">Default template to vue3 application</h1>
+<p>In this template we add special loader which shown while vue application don't applied to #page-app</p>
+<?php ob_start() ?>
+    <div style="padding: 30px; border-radius: 10px; background: #EEE;">
+        {{ content }}
+    </div>
+<?php IncludeCom('_dev/page_vue3_app_template', ['content' => ob_get_clean()]) ?>
+
+<script type="module">
+    import { formatTimer } from "/i/js/_dev/format_date.js";
+
+    const loadingStopper = () => {
+        let sum = 0;
+        for (let i = 0; i < 1e9; i++) {
+            sum += i;
+        }
+    };
+    loadingStopper();
+
+    Vue.createApp({
+        data() {
+            return {
+                helloString: 'Hello world!',
+                content: 'Loading...',
+            }
+        },
+        async mounted() {
+            await this.emulateAjax();
+            this.content = this.helloString + formatTimer(+Date.now());
+        },
+        methods: {
+            formatTimer(seconds) {
+                return formatTimer(seconds, false);
+            },
+            async emulateAjax() {
+                const delay = ms => new Promise(
+                    resolve => setTimeout(resolve, ms)
+                ) ;
+                await delay(3000);
+            },
+        },
+    }).mount('#page-app');
+</script>
